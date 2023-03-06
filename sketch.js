5c92ce1d895b7e7fa310cd9ada6c08aa3c2a9344
aketkar18 <anujketkar1118@gmail.com>
2023-03-06 14:10:12 -0600

    swipe

--- sketch.js
+++ sketch.js
@@ -1,16 +1,41 @@
-let button, img;
+let homePageImg, menuImg, moveVid, currentPage;
+let dragStartY = 0;
+let dragEndY = 0;
 
 function preload() {
   homePageImg = loadImage('assets/homepage.png');
   menuImg = loadImage('assets/menu.png');
+  moveVid = createVideo('assets/menu.mp4');
 }
 
 function setup() {
   createCanvas(1500, 3000);
-  loadHomePage();
+  currentPage = "home";
+  image(homePageImg, 0, 0);
+  // moveVid.hide();
 }
 
-function loadHomePage(){
-  image(homePageImg, 0, 0, 450, 900);
+function draw() {
+  background(0);
+  if (currentPage === "home") {
+    image(homePageImg, 0, 0);
+  } else if (currentPage === "menu") {
+    image(menuImg, 0, 0);
+  }
 }
 
+function mousePressed() {
+  dragStartY = mouseY;
+  // dragEndY = mouseY;
+}
+
+function mouseDragged() {
+  dragEndY = mouseY;
+}
+
+function mouseReleased() {
+  if (dragStartY > dragEndY && dragStartY - dragEndY > 100 && currentPage === "home") {
+    moveVid.play();
+    // currentPage = "menu";
+  }
+}
