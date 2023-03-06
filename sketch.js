f2e205188cfa633aad718d5db0d9d96feb1a2447
aketkar18 <anujketkar1118@gmail.com>
2023-03-06 14:19:04 -0600

    swipes working

--- sketch.js
+++ sketch.js
@@ -1,18 +1,16 @@
-let homePageImg, menuImg, moveVid, currentPage;
-let dragStartY = 0;
-let dragEndY = 0;
+let homePageImg, menuImg, currentPage;
+let touchStartY = 0;
+let touchEndY = 0;
 
 function preload() {
   homePageImg = loadImage('assets/homepage.png');
   menuImg = loadImage('assets/menu.png');
-  moveVid = createVideo('assets/menu.mp4');
 }
 
 function setup() {
   createCanvas(1500, 3000);
   currentPage = "home";
   image(homePageImg, 0, 0);
-  // moveVid.hide();
 }
 
 function draw() {
@@ -24,18 +22,22 @@ function draw() {
   }
 }
 
-function mousePressed() {
-  dragStartY = mouseY;
-  // dragEndY = mouseY;
+function touchStarted() {
+  touchStartY = mouseY;
+  touchEndY = mouseY;
 }
 
-function mouseDragged() {
-  dragEndY = mouseY;
+function touchMoved() {
+  touchEndY = mouseY;
 }
 
-function mouseReleased() {
-  if (dragStartY > dragEndY && dragStartY - dragEndY > 100 && currentPage === "home") {
-    moveVid.play();
-    // currentPage = "menu";
+function touchEnded() {
+  let swipeDistance = touchEndY - touchStartY;
+  if (swipeDistance > 100 && currentPage === "menu") {
+    currentPage = "home";
+    image(homePageImg, 0, 0);
+  } else if (swipeDistance < -100 && currentPage === "home") {
+    currentPage = "menu";
+    image(menuImg, 0, 0);
   }
 }
